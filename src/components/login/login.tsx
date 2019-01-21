9b9187c72412f20a2500fb9ca68d9a0638251d84
TwoPlusTwoOne <francisco.digiandomenico@gmail.com>
2019-01-20 21:12:43 -0300

    Modified the log in a bit Removed database folder Install express types Add `sql-injection-queries.txt` Add `api.ts`

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,15 +1,53 @@
 import * as React from 'react'
-import { Button, TextField } from '@material-ui/core'
+import { ChangeEvent } from 'react'
+import { TextField } from '@material-ui/core'
+import { getAllXss, getUserById } from '../../api/api'
+import { RegisterButton } from './registerButton'
+import { LoginButton } from './loginButton'
 
 export type Props = {}
 
-export class Login extends React.PureComponent<Props> {
+export type State = {
+  username: string
+  value: string
+}
+
+export class Login extends React.PureComponent<Props, State> {
+  state = {
+    username: '',
+    value: '',
+  }
+
+  componentDidMount(): void {
+    getAllXss().then(response => response.json()).then(value => this.setState({
+      ...this.state,
+      value: value.results[0].value
+    }))
+  }
+
+  handleClickRegister = () => {
+    getUserById(this.state.username).then(response => response.json()).then(console.log).catch(console.log)
+  }
+
+  handleClickLogin = () => {
+
+  }
+
+  handleChange = (field: string, e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+    const value = e.target.value
+    this.setState({ ...this.state, [field]: value })
+  }
+
   render() {
+    const { value } = this.state
+    console.log({ value })
     return (
       <div className={'login-component'}>
         <TextField
           placeholder={'Username'}
           variant={'outlined'}
+          value={this.state.username}
+          onChange={this.handleChange.bind(null, 'userId')}
         />
         <TextField
           placeholder={'Password'}
@@ -17,9 +55,10 @@ export class Login extends React.PureComponent<Props> {
           variant={'outlined'}
         />
         <div className={'login-component-buttons'}>
-          <Button onClick={() => console.log('Click login')} size="medium" variant="contained" color="default">Register</Button>
-          <Button onClick={() => console.log('Click login')} size="medium" variant="contained" color="primary">Login</Button>
+          <RegisterButton onClick={this.handleClickRegister} />
+          <LoginButton onClick={this.handleClickLogin} />
         </div>
+        <div />
       </div>
     )
   }
