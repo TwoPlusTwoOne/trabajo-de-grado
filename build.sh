50c2e7dca351d5f15601126eaa87a278229611fc
FranciscoCeriani <francisco.ceriani@mulesoft.com>
2019-01-21 22:23:07 -0300

    npm starts on steroids

---
+++ build.sh
@@ -0,0 +1,14 @@
+center() {
+  termwidth="$(tput cols)"
+  padding="$(printf '%0.1s' ={1..500})"
+  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+
+center "Installing project dependencies"
+npm i
+center "Installing server dependencies"
+npm i --prefix packages/server
+center "Installing client dependencies"
+npm i --prefix packages/client
+center "Runing project"
+concurrently --kill-others "npm start --prefix packages/server" "PORT=8080 npm run start --prefix packages/client"
