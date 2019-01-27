1eaa22bb5e74fbf4f2b2a32c806fd38ee2b0c8f1
FranciscoCeriani <francisco.ceriani@mulesoft.com>
2019-01-27 13:29:05 -0300

    basic topbar added

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,11 +3,12 @@ import './App.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { LoginView } from './views/loginView'
 import { Container } from './components/catalog/container/container'
+import Topbar  from './components/topbar/tobar'
  
 const App = () => (
   <Router>
     <div>
-      <Route path="/" exact component={Container} />
+      <Route path="/" exact component={Topbar} />
     </div>
   </Router>
 )
