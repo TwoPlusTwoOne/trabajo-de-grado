6fbaadc36029f2244dc6f2201f0ec0b2064b3cc1
TwoPlusTwoOne <francisco.digiandomenico@gmail.com>
2019-07-18 14:04:41 -0300

    (App) NOW the base works with the redirects correctly

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -5,21 +5,20 @@ import { SecuredRoute } from './components/secured-route/securedRoute'
 import { Home } from './components/home/home'
 import { LoginView } from './views/login-view/loginView'
 import { UnsecuredRoute } from './components/unsecured-route/unsecuredRoute'
-import { SuperComponent } from './components/superComponent/superComponent'
 import { ProductView } from './views/product-view/productView'
 import { Base } from './components/base/base'
 
 const App = () => {
   return (
-    <Router>
+    <Base>
+      <Router>
       <Switch>
-        <Base>
-          <UnsecuredRoute exact path="/login" component={LoginView} />
-          <SecuredRoute exact path="/" component={Home} />
-          <SecuredRoute exact path="/products/:productId" component={ProductView} />
-        </Base>
+        <UnsecuredRoute exact path="/login" component={LoginView} />
+        <SecuredRoute exact path="/" component={Home} />
+        <SecuredRoute exact path="/products/:productId" component={ProductView} />
       </Switch>
     </Router>
+    </Base>
   )
 }
 
